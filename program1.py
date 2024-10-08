17a4ce9008b5ba3eb1af11eb4752376e02e427ca
Aman patel <125423376+Amanpatel83@users.noreply.github.com>
2024-10-08 14:33:03 +0530

    Update program1.py
    Aman Patel p1

--- program1.py
+++ program1.py
@@ -4,7 +4,26 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
-        pass
+        stack = []
+        mapping = {')': '(', '}': '{', ']': '['}
+
+        # Traverse through each character in the string
+        for char in s:
+            # If the character is a closing bracket
+            if char in mapping:
+                # Pop the top element from the stack if it's not empty, otherwise assign a dummy value '#'
+                top_element = stack.pop() if stack else '#'
+
+                # Check if the popped element is the corresponding opening bracket
+                if mapping[char] != top_element:
+                    return False
+            else:
+                # If it's an opening bracket, push it onto the stack
+                stack.append(char)
+
+        # If the stack is empty, all brackets were matched correctly
+        return not stack
+
 
 
 
